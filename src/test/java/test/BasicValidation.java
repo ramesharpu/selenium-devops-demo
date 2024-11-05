4c92b2322a1fa221c2dbb9e36ba9b9fe14f6dae5
Ramesh A <ramesharpu@gmail.com>
2024-11-05 19:26:00 +0530

    updated the file

--- src/test/java/test/BasicValidation.java
+++ src/test/java/test/BasicValidation.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Reporter;
@@ -18,55 +19,47 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BasicValidation {
-	
+
 	public static WebDriver driver;
-	boolean grid = true;
-	String gridUrl = "http://localhost:4444";
-	
+
 	@BeforeTest
 	public void beforeTest() {
-		if(grid) {
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setBrowserName("chrome");
-			try {
-				driver = new RemoteWebDriver(new URI(gridUrl).toURL(), cap);
-			} catch (MalformedURLException | URISyntaxException e) {
-				e.printStackTrace();
-			}
-		}
-		else
-			driver = new ChromeDriver();
-		
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		driver = new ChromeDriver();
+
 		driver.get("https://www.facebook.com");
 	}
-	
+
 	@AfterTest
 	public void afterTest() {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void titleValiadation() {
 		String expectedResult = "Facebook – log in or sign up";
 		String actualResult = driver.getTitle();
-		
+
 		Reporter.log("Expected Result = " + expectedResult);
 		Reporter.log("Actual Result = " + actualResult);
-		
+
 		assertTrue(actualResult.equals(expectedResult), "Mismatch in the page title,");
 	}
 
-	
 	@Test
 	public void welcomeMessageValiadation() {
 		String expectedResult = "Facebook helps you connect and share with the people in your life.";
-		
+
 		WebElement elem = driver.findElement(By.xpath("//h2[@class='_8eso']"));
 		String actualResult = elem.getText();
-		
+
 		Reporter.log("Expected Result = " + expectedResult);
 		Reporter.log("Actual Result = " + actualResult);
-		
+
 		assertTrue(actualResult.equals(expectedResult), "Mismatch in the page title,");
 	}
 }
